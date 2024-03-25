c97567e7689f9df99b8bcf052e9895b8c75551c6
Durga Prasad <your.dporange.siripuram007@gmail.com>
2024-03-25 19:39:39 +0530

    Your commit message

---
+++ resume react/my-app/src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <header>
+    <h1>durga prasad</h1>
+    </header>
+    <pre>
+    name:s.durga prasad
+    <br></br>
+    college name:vkr vnb&agk enginering college
+    <br></br>
+    gudivada
+    </pre>
+    <fieldset>
+        <center>
+        <h1>RESUME</h1>
+    </center>
+    <h2>SIRIPURAM DURGA PRASAD</h2>
+    <h3>carrer objective:-</h3>
+    <p>
+      As a fresher, I want to put to use my creative skills, technologies , innovative thoughts that will benefit the 
+     Organization in the long run and also help me in building my professional career.
+    </p>
+    <h3>technical skills:-</h3>
+    <pre>
+    1.htmal<br></br>
+    2.css<br></br>
+    3.javascript<br></br>
+    4.react js<br></br>
+    5.sql
+    </pre>
+    <h3>hobbies:-</h3>
+    <pre>
+    1.listening music<br></br>
+    2.playing cricket<br></br>
+    3.reading books
+    </pre>
+    <h3>declaration:-</h3>
+    <p>All the information above in the resume is correst to the best of my knowledge.........</p>
+    <pre>
+    NAME: S.durga prasad<br></br>
+    DATE:05/12/2023<br></br>
+    ADRESS:SRNAGAR HYDERABAD
+    </pre>
+    </fieldset> 
+    
+    </div>
+          
+
+  );
+}
+
+export default App;
